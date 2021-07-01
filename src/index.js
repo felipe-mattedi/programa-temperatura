8de44e4e5994c122e60d5ef2930d545c6624f29b
Felipe Mattedi <felipe.mattedi@terra.com.br>
2021-06-30 21:57:52 -0300

    Corrige sintaxe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable import/extensions */
 import input from 'readline-sync';
-import consultaWeather from './downloadInfo.js'
-import salvarPrevisao from './salvarPrevisao.js'
+import downloadInfo from './downloadInfo.js';
+import salvarPrevisao from './salvarPrevisao.js';
 
-function msginicializacao(){
+function msginicializacao() {
   console.log(`
   *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
   Olá! Seja bem vindo ao programa que te mostra a temperatura 
@@ -10,60 +11,48 @@ function msginicializacao(){
   Digite a cidade desejada e a quantidade de dias que você deseja 
   ver a temperatura
   *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-  `)
-} 
+  `);
+}
 
-function validacidade(){
-  let cidade = input.question('Digite a cidade desejada: ')
-  if (cidade == ''){
-    throw new Error("Digite uma cidade valida")
-  }
-  else{
-    return cidade
+function validacidade() {
+  const cidade = input.question('Digite a cidade desejada: ');
+  if (cidade === '') {
+    throw new Error('Digite uma cidade valida');
+  } else {
+    return cidade;
   }
 }
 
-function validadias(){
-  let qtd = input.question('Digite a quantidade de dias (1 ate 7): ')
-  if (parseInt(qtd) == NaN || parseInt(qtd) < 1 || parseInt(qtd) > 7){
-    throw new Error("Digite uma quantidade de dias válida")
-  }
-  else{
-    return qtd
+function validadias() {
+  const qtd = input.question('Digite a quantidade de dias (1 ate 7): ');
+  if (Number(qtd).isNaN() || Number(qtd) < 1 || Number(qtd) > 3) {
+    throw new Error('Digite uma quantidade de dias válida');
   }
 }
 
-msginicializacao()
+msginicializacao();
 
 async function main() {
-
-  try{
-  
-    let cidade = validacidade()
-    let qtd = validadias()
-    let retorno = await consultaWeather(cidade,qtd)
-    await salvarPrevisao(retorno)
-    console.log(retorno)
-  
-  }
-  
-  catch(e) {
+  try {
+    const cidade = validacidade();
+    const qtd = validadias();
+    const retorno = await downloadInfo(cidade, qtd);
+    await salvarPrevisao(retorno);
+    console.log(retorno);
+  } catch (e) {
     console.log(`
     *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
     Erro durante a execução do programa:
     ${e.message}
     *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-    `)
-  }
-  
-  finally{
+    `);
+  } finally {
     console.log(`
     *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
     Termino da execucao do programa
     *=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
-    `)
+    `);
   }
-
 }
 
-main()
+main();
